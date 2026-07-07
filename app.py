1e8aeb0b7c508cd45a9622860f4111b9ac6e27cd
charanv9 <charanvamsi94@gmail.com>
2026-07-07 17:21:13 +0530

    feat:add delete-task feature

--- app.py
+++ app.py
@@ -13,6 +13,13 @@ def update_task(index, new_title):
         return True
     return False
 
+def delete_task(index):
+    if 0 <= index < len(tasks):
+        tasks.pop(index)
+        return True
+    return False
+
 create_task("Complete ISP Task 1")
 update_task(0, "Complete ISP Task 1 Successfully")
+delete_task(0)
 print(list_tasks())
