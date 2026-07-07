a34f3ea09000af185d8398ed3ce8e07b34017abf
charanv9 <charanvamsi94@gmail.com>
2026-07-07 17:09:53 +0530

    feat:add update-task feature

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ tasks = []
 def create_task(title):
     tasks.append({"title": title, "status": "todo"})
     return tasks
+
 def list_tasks():
     return tasks
 
+def update_task(index, new_title):
+    if 0 <= index < len(tasks):
+        tasks[index]["title"] = new_title
+        return True
+    return False
+
 create_task("Complete ISP Task 1")
+update_task(0, "Complete ISP Task 1 Successfully")
 print(list_tasks())
